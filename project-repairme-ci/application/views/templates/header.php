8636046660d62b7cb1207fdd862afcd68af4e94f
BagusDuwiPrasetiyo <bagusduwi9@gmail.com>
2020-04-30 14:56:42 +0700

    delete assets

--- project-repairme-ci/application/views/templates/header.php
+++ project-repairme-ci/application/views/templates/header.php
@@ -4,44 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--  
-    Document Title
-    =============================================
-    -->
     <title><?= $judul; ?></title>
-    <!--  
-    Favicons
-    =============================================
-    -->
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url(); ?>assets/home-master/assets/images/favicons/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= base_url(); ?>assets/home-master/assets/images/favicons/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-    <!--  
-    Stylesheets
-    =============================================
-    
-    -->
-    <!-- Default stylesheets-->
+
     <link href="<?= base_url(); ?>assets/home-master/assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Template specific stylesheets-->
+
+    <!-- Templates-->
+    
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    
     <link href="<?= base_url(); ?>assets/home-master/assets/lib/animate.css/animate.css" rel="stylesheet">
     <link href="<?= base_url(); ?>assets/home-master/assets/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <link href="<?= base_url(); ?>assets/home-master/assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
@@ -50,12 +23,15 @@
     <link href="<?= base_url(); ?>assets/home-master/assets/lib/owl.carousel/dist/<?= base_url(); ?>assets/home-master/assets/owl.theme.default.min.css" rel="stylesheet">
     <link href="<?= base_url(); ?>assets/home-master/assets/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
     <link href="<?= base_url(); ?>assets/home-master/assets/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+    
     <!-- Main stylesheet and color file-->
     <link href="<?= base_url(); ?>assets/home-master/assets/css/style.css" rel="stylesheet">
     <link id="color-scheme" href="<?= base_url(); ?>assets/home-master/assets/css/colors/default.css" rel="stylesheet">
+    
     <!-- untuk registrasi pelanggan -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+
      <!-- untuk leafletjs -->
      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
       integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
@@ -68,6 +44,8 @@
       <link rel="stylesheet" type="text/css" href="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.css">
       <script src="<?= base_url(); ?>assets/js/Control.Coordinates.js"></script>
       <script src="<?= base_url(); ?>assets/js/petaLokasi.js"></script>
+
+      <!-- untuk jquery -->
       <script src="<?= base_url(); ?>assets/js/jquery-3.4.1.js"></script>
 
   </head>
