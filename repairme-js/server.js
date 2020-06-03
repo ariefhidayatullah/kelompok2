53db667832801231393edf50b53b19cdc76b79f3
BagusDuwiPrasetiyo <bagusduwi9@gmail.com>
2020-06-03 11:13:01 +0700

    :home => view pertama + static file

--- repairme-js/server.js
+++ repairme-js/server.js
@@ -17,8 +17,11 @@ app.set('view engine', 'ejs');
 
 //set views ke 
 app.set('views', path.join(__dirname, 'views'));
-app.use('/cssfile', express.static(__dirname + '/assets/css'));
+app.use('/css', express.static(__dirname + '/assets/css'));
 app.use('/vue', express.static(__dirname + '/assets/js/vueJs/vue.js'));
+app.use('/lib', express.static(__dirname + '/assets/lib'));
+app.use('/images', express.static(__dirname + '/assets/images'));
+app.use('/js', express.static(__dirname + '/assets/js'));
 
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
@@ -31,7 +34,7 @@ mongoose.connect(dbConfig.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
@@ -42,7 +45,7 @@ mongoose.connect(dbConfig.url, {
 require('./app/routes/home.routes')(app);
 require('./app/routes/api.router/api-mitra.routes.js')(app);
 
-app.get('/favicon.ico', (req,res) => {
+app.get('/favicon.ico', (req, res) => {
     res.status(204);
 });
 
