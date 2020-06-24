f905862c384cbc61d836218dc0f74613e933db0c
Alvin-kor <55471978+Alvin-kor@users.noreply.github.com>
2020-06-24 18:41:23 +0700

    update

--- repairme-react/ProjectAndroidReact/config/config.js
+++ repairme-react/ProjectAndroidReact/config/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://192.168.1.8:3000`
+  baseURL: `http://192.168.0.104:3000`
 });
 //const BaseUrl = "http://192.168.1.8:3000";
