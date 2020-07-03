1f51e3db9473823836e4fa34206ed7bc3de84757
arfhdytllh <55471978+Alvin-kor@users.noreply.github.com>
2020-07-03 16:03:47 +0700

    riwayat perbaikan mitra handphone dan laptop

--- repairme-js/app/routes/mitra.routes.js
+++ repairme-js/app/routes/mitra.routes.js
@@ -5,19 +5,19 @@ module.exports = (app, express) => {
     const router = express.Router();
 
     router.get('/', (req, res, next) => {
-       if (req.session.user) {
+        if (req.session.user) {
             if (req.session.user.jenis === 'mitra') {
                 res.render('mitra/index', {
-                judul: 'Dashboard Mitra',
-                email: req.session.user.email,
-                content: 'mitra/test'
+                    judul: 'Dashboard Mitra',
+                    email: req.session.user.email,
+                    content: 'mitra/test'
                 })
-            }else {
+            } else {
                 next()
             }
-       }else{
+        } else {
             res.redirect('/login');
-       }
+        }
     });
 
     router.get('/registrasi', (req, res) => {
@@ -30,15 +30,15 @@ module.exports = (app, express) => {
         if (req.session.user) {
             if (req.session.user.jenis === 'mitra') {
                 res.render('mitra/profile/index', {
-                judul: 'Profile Mitra',
-                email: req.session.user.email
+                    judul: 'Profile Mitra',
+                    email: req.session.user.email
                 })
-            }else {
+            } else {
                 next()
             }
-       }else{
+        } else {
             res.redirect('/login');
-       }
+        }
     });
 
     router.get('/pengajuan/:jenis', (req, res) => {
@@ -46,38 +46,62 @@ module.exports = (app, express) => {
             if (req.session.user.jenis === 'mitra') {
                 if (req.params.jenis === "laptop") {
                     res.render('mitra/perbaikan/pengajuan/laptop', {
-                    judul: 'Pengajuan Perbaikan',
-                    email: req.session.user.email
+                        judul: 'Pengajuan Perbaikan',
+                        email: req.session.user.email
                     })
-                }else if(req.params.jenis === "handphone"){
+                } else if (req.params.jenis === "handphone") {
                     res.render('mitra/perbaikan/pengajuan/handphone', {
-                    judul: 'Pengajuan Perbaikan',
-                    email: req.session.user.email
+                        judul: 'Pengajuan Perbaikan',
+                        email: req.session.user.email
                     })
-                }else {
+                } else {
                     next()
                 }
-            }else {
+            } else {
                 next()
             }
-       }else{
+        } else {
             res.redirect('/login');
-       }
+        }
+    })
+
+    router.get('/riwayat/:jenis', (req, res) => {
+        if (req.session.user) {
+            if (req.session.user.jenis === 'mitra') {
+                if (req.params.jenis === "laptop") {
+                    res.render('mitra/perbaikan/riwayat/laptop', {
+                        judul: 'Riwayat Perbaikan',
+                        email: req.session.user.email
+                    })
+                } else if (req.params.jenis === "handphone") {
+                    res.render('mitra/perbaikan/riwayat/handphone', {
+                        judul: 'Riwayat Perbaikan',
+                        email: req.session.user.email
+                    })
+                } else {
+                    next()
+                }
+            } else {
+                next()
+            }
+        } else {
+            res.redirect('/login');
+        }
     })
 
     router.get('/voucher', (req, res) => {
         if (req.session.user) {
             if (req.session.user.jenis === 'mitra') {
                 res.render('mitra/perbaikan/voucher/index', {
-                judul: 'Terima Voucher',
-                email: req.session.user.email
+                    judul: 'Terima Voucher',
+                    email: req.session.user.email
                 })
-            }else {
+            } else {
                 next()
             }
-       }else{
+        } else {
             res.redirect('/login');
-       }
+        }
     })
 
     router.get('/perbaikan/:jenis', (req, res) => {
@@ -85,21 +109,21 @@ module.exports = (app, express) => {
             if (req.session.user.jenis === 'mitra') {
                 if (req.params.jenis === 'laptop') {
                     res.render('mitra/perbaikan/perbaikan/laptop', {
-                    judul: 'Perbaikan Laptop',
-                    email: req.session.user.email
+                        judul: 'Perbaikan Laptop',
+                        email: req.session.user.email
                     })
-                }else if (req.params.jenis === 'handphone') {
+                } else if (req.params.jenis === 'handphone') {
                     res.render('mitra/perbaikan/perbaikan/handphone', {
-                    judul: 'Perbaikan Handphone',
-                    email: req.session.user.email
+                        judul: 'Perbaikan Handphone',
+                        email: req.session.user.email
                     })
                 }
-            }else {
+            } else {
                 next()
             }
-       }else{
+        } else {
             res.redirect('/login');
-       }
+        }
     })
 
     router.get('/dibatalkan/:jenis', (req, res) => {
@@ -107,21 +131,21 @@ module.exports = (app, express) => {
             if (req.session.user.jenis === 'mitra') {
                 if (req.params.jenis === 'laptop') {
                     res.render('mitra/perbaikan/dibatalkan/laptop', {
-                    judul: 'Perbaikan Dibatalkan',
-                    email: req.session.user.email
+                        judul: 'Perbaikan Dibatalkan',
+                        email: req.session.user.email
                     })
-                }else if (req.params.jenis === 'handphone') {
+                } else if (req.params.jenis === 'handphone') {
                     res.render('mitra/perbaikan/dibatalkan/handphone', {
-                    judul: 'Perbaikan Dibatalkan',
-                    email: req.session.user.email
+                        judul: 'Perbaikan Dibatalkan',
+                        email: req.session.user.email
                     })
                 }
-            }else {
+            } else {
                 next()
             }
-       }else{
+        } else {
             res.redirect('/login');
-       }
+        }
     })
 
     router.get('/selesai/:jenis', (req, res) => {
@@ -129,21 +153,21 @@ module.exports = (app, express) => {
             if (req.session.user.jenis === 'mitra') {
                 if (req.params.jenis === 'laptop') {
                     res.render('mitra/perbaikan/selesai/laptop', {
-                    judul: 'Perbaikan Selesai',
-                    email: req.session.user.email
+                        judul: 'Perbaikan Selesai',
+                        email: req.session.user.email
                     })
-                }else if (req.params.jenis === 'handphone') {
+                } else if (req.params.jenis === 'handphone') {
                     res.render('mitra/perbaikan/selesai/handphone', {
-                    judul: 'Perbaikan Selesai',
-                    email: req.session.user.email
+                        judul: 'Perbaikan Selesai',
+                        email: req.session.user.email
                     })
                 }
-            }else {
+            } else {
                 next()
             }
-       }else{
+        } else {
             res.redirect('/login');
-       }
+        }
     })
 
     app.use('/mitra', router);
